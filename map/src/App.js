fbc4bc1246639649a50cad3b5cad1b5ec9d03352
Christophe Viau <christopheviau@gmail.com>
2024-03-20 17:15:37 -0400

    more geojsons

--- map/src/App.js
+++ map/src/App.js
@@ -1,8 +1,11 @@
 import React, { useRef, useEffect, useState } from 'react';
 import mapboxgl from '!mapbox-gl'; // eslint-disable-line import/no-webpack-loader-syntax
 
+import geojsonFile from './assets/us-states.geojson';
+// import geojsonFile from './assets/map_flat.geojson';
+// import geojsonFile from './assets/HRVA_Units_land_4326.json';
+
 import './App.css';
-// import geojson from './us-states';
 mapboxgl.accessToken = 'pk.eyJ1IjoiY2hyaXN2aWF1IiwiYSI6ImFERnVTeXcifQ.Eun4jtPE8upbuP2uKawtFw';
 
 function App() {
@@ -11,6 +14,8 @@ function App() {
   const [lng, setLng] = useState(-96);
   const [lat, setLat] = useState(37);
   const [zoom, setZoom] = useState(4);
+
+  console.log(geojsonFile)
 
   useEffect(() => {
     if (map.current) return;
@@ -22,20 +27,46 @@ function App() {
     });
 
     map.current.on('load', () => {
-      map.current.addSource('us', {
+      map.current.addSource('geo', {
           type: 'geojson',
-          data: 'https://cdn.jsdelivr.net/gh/ericjames/us-states-geojson/all-states/NY.geo.json'
+          data: geojsonFile
       });
 
       map.current.addLayer({
-          'id': 'us-layer',
+          'id': 'geo-layer',
           'type': 'fill',
-          'source': 'us',
+          'source': 'geo',
           'paint': {
-            'fill-color': 'red',
-            'fill-opacity': 0.2
-          }
+            'fill-color': {
+              property: 'density',
+              stops: [[0, '#fff'], [500, '#e00'], [1200, '#f00']]
+            },
+            'fill-opacity': 0.8
+          },
       });
+
+      // map.current.addLayer({
+      //   'id': 'geo-layer',
+      //   'type': 'fill',
+      //   'source': 'geo',
+      //   'paint': {
+      //     'fill-color': 'orange',
+      //     'fill-opacity': 0.8
+      //   },
+      // });
+
+      // map.current.addLayer({
+      //   'id': 'geo-borders',
+      //   'type': 'line',
+      //   'source': 'geo',
+      //   'layout': {},
+      //   'paint': {
+      //       'line-color': 'red',
+      //       'line-width': 2
+      //   }
+      // });
+
+
     });
 
   });
